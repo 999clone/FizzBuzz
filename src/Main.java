ed38ac1464d99cc89b7a85e997d5f2c8732d6607
clone <shervinrajabi2016@gmail.com>
2025-03-22 22:54:23 +0330

    completed the Main Class and add some improvements such as asking for the end number and using the start number so it can be changeable for future.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int start = 1;
+        System.out.println("Please enter the end number: ");
+        int end = sc.nextInt();
+
+        for (int i = start; i <= end; i++) {
+            String temp = "";
+
+            if (i % 3 == 0)
+                temp += "Fizz";
+            if (i % 5 == 0)
+                temp += "Buzz";
+            if (temp == "")
+                temp += i;
+
+            System.out.println(temp);
+        }
     }
 }
